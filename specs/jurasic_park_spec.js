34ebeb85e53167623a7eb1d181c38aa8e82c09a4
R.Murzakov <romuald.murzakov@gmail.com>
2017-08-29 16:46:36 +0100

    Park is created

--- specs/jurasic_park_spec.js
+++ specs/jurasic_park_spec.js
@@ -7,12 +7,19 @@ describe("Jurasic park", function(){
   beforeEach(function(){
     dinosaur1 = new Dinosaur("Triceratops", 5);
     park = new Park();
+    console.log(park.enclosure.length);
   })
 
   it("Check dinsaur1", function(){
     assert.equal(dinosaur1.type, "Triceratops");
     assert.equal(dinosaur1.offsprings, 5);
   })
+
+  it("Check enclosure", function() {
+    assert.equal(park.enclosure.length, 0);
+  })
+
+
 
 
 })
